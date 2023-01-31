cfc28ce007763115e36154ed7227f50e3847ba9d
tahsinraza95 <tahsinr693@gmail.com>
2023-02-01 01:11:31 +0530

    Added: Using Switch Case

---
+++ Employee Wage by Switch case/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Employee_Wage_by_Switch_case
+{
+    internal class Program
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        static void Main(string[] args)
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            Random random= new Random();
+            int empCheck = random.Next(0, 2);
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break; 
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default: 
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+    }
+}
